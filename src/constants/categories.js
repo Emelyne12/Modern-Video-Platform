701b2ba2174ad8bf7697775556afc9eda106b7e4
Emelyne12 <ingabiremelyne1234@gmail.com>
2026-03-15 20:28:40 +0200

    feat: define sidebar and pill categories

---
+++ src/constants/categories.js
@@ -0,0 +1,27 @@
+export const categories = [
+  'All',
+  'Coding',
+  'Music',
+  'Gaming',
+  'News',
+  'Live',
+  'Sports',
+  'Movies',
+  'Podcasts',
+  'Education',
+  'React',
+  'JavaScript',
+  'Design',
+  'AI',
+]
+
+export const sidebarCategories = [
+  'Home',
+  'Trending',
+  'Music',
+  'Gaming',
+  'Live',
+  'News',
+  'Sports',
+  'Learning',
+]
